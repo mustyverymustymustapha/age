4597cf62a6b2e19c9ebca923de1e523f3f08a3c7
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-06-24 14:14:18 +0000

    Second commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+const dobInput = document.getElementById('inputDob');
+const currentDateInput = document.getElementById('cdate');
+const currentAgeDiv = document.getElementById('currentAge');
+
+
+function getDOB() {
+    const dob = new Date(dobInput.value);
+    const currentDate = new Date(currentDateInput.value);
+
+
+    const ageInMillis = currentDate - dob;
+
+  
+    const ageDate = new Date(ageInMillis);
+    const years = ageDate.getUTCFullYear() - 1970;
+    const months = ageDate.getUTCMonth();
+    const days = ageDate.getUTCDate();
+
+    
+    currentAgeDiv.textContent = `You age is ${years} years, ${months} months, and ${days} days.`;
+}
+
